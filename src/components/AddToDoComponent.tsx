c3f49528d39b4c61bdd23e153640b1a171fe01f7
tom-illy <pppike.0131@gmail.com>
2021-02-24 22:33:40 +0900

    [Update]タスク登録画面にタイトルと内容を追加

--- src/components/AddToDoComponent.tsx
+++ src/components/AddToDoComponent.tsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { useAddToDoHook } from 'hooks/addToDoHook'
+import { useAddToDoHook } from "hooks/addToDoHook";
 
 export const AddToDo: React.FC = () => {
-  const addToDoHook = useAddToDoHook()
+  const addToDoHook = useAddToDoHook();
 
   return (
     <>
-    { addToDoHook.addToDo.isOpen ? (
-      <>
-      <div
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-      >
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+      {addToDoHook.addToDo.isOpen ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between items-center p-5 border-b border-solid border-gray-300 rounded-t">
-                  <h3 className="text-2xl font-semibold">
-                    ToDoを登録する
-                  </h3>
+                  <h3 className="text-2xl font-semibold">ToDoを登録する</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black float-right text-2xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => {console.log('click2'); addToDoHook.addToDo.updateOpen(false);}}
+                    onClick={() => {
+                      console.log("click2");
+                      addToDoHook.addToDo.updateOpen(false);
+                    }}
                   >
                     <span className="bg-transparent text-black h-6 w-6 text-4xl">
                       ×
@@ -29,13 +28,20 @@ export const AddToDo: React.FC = () => {
                   </button>
                 </div>
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-gray-600 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
+                  <div className="flex flex-col">
+                    <label className="leading-loose">タイトル</label>
+                    <input
+                      type="text"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="タイトルを入力してください"
+                    />
+                    <label className="leading-loose">内容</label>
+                    <input
+                      type="text"
+                      className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                      placeholder="内容を入力してください"
+                    />
+                  </div>
                 </div>
                 <div className="flex items-center justify-between p-6 border-t border-solid border-gray-300 rounded-b">
                   <button
@@ -54,13 +60,13 @@ export const AddToDo: React.FC = () => {
                   >
                     登録
                   </button>
-                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-          </>
-    ) : null }
+        </>
+      ) : null}
     </>
   );
 };
